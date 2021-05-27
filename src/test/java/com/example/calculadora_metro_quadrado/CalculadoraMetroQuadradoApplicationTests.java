6794c944f3acda42ae80da1f90ed5802399e59d2
Caique Noboa <caique.noboa@mercadolivre.com>
2021-05-27 12:21:07 -0300

    Calculadora Metro Quadrado

---
+++ src/test/java/com/example/calculadora_metro_quadrado/CalculadoraMetroQuadradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculadora_metro_quadrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraMetroQuadradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
